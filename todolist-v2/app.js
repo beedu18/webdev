a070791755ab62f167faac1ebbcf4107e160ac1a
beedu <b1n4y4k@gmail.com>
2020-10-06 21:39:55 +0530

    database added. delete button works.

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -1,21 +1,71 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname+"/date.js");
+// const date = require(__dirname+"/date.js");
+const mongoose = require("mongoose");
 
+//initialize express
 const app = express();
 
-let items = [];
-let workItems = [];
-
+//use the modules
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+//connect to database
+mongoose.connect("mongodb://localhost:27017/todolistDB", 
+    { useNewUrlParser: true, useUnifiedTopology: true });
+
+//create schema
+const itemSchema = {
+    name: {
+        type: String,
+        required: [true, "No item specified"]
+    }
+};
+
+//model the database
+const Item = mongoose.model("Item", itemSchema);
+
+//make dummy documents
+const item1 = new Item({
+    name: "You can Edit an entry"
+});
+
+const item2 = new Item({
+    name: "Delete it once it's done"
+});
+
+const item3 = new Item({
+    name: "Or strike it off"
+});
+
+//configure routes
 app.get("/", (req, res) => {
-    const today = date.getDate();
-    
-    // searches for list.ejs template in views/ directory
-    res.render("list", {header: today, newItems: items}); 
+    // const today = date.getDate();
+    Item.find({}, (err, items) => {
+        if(err)
+            console.log(err);
+        else {
+            // console.log(items);
+            if(items.length===0) {
+                //add dummy items to the collection
+                Item.insertMany([item1, item2, item3], (err) => {
+                    if(err)
+                        console.log(err);
+                    else
+                        console.log("dummy entry successful");
+                });
+
+                //redirect to home route again to show
+                res.redirect("/"); 
+            }
+            else {
+                // searches for list.ejs template in views/ directory
+                res.render("list", {header: "Today", newItems: items}); 
+            }
+        }    
+    });
+    // res.send("hello"); 
 });
 
 app.post("/", (req, res)=> {
@@ -26,11 +76,28 @@ app.post("/", (req, res)=> {
             res.redirect("/work");
         }
         else {
-            items.push(item);
+            const newItemDocument = new Item({
+                name: item
+            });
+            newItemDocument.save();
+            // items.push(item);
             res.redirect("/");
         }   
     }
-    console.log(item);
+    // console.log(item);
+});
+
+app.post("/delete", (req, res) => {
+    // console.log(req.body)
+    let id = req.body.id;
+    Item.findByIdAndRemove(id, err => {
+        if(err)
+            console.log(err);
+        else {
+            // console.log("Deleted Successfully");
+            res.redirect("/");
+        }
+    });
 });
 
 app.get("/work", (req, res) => {
@@ -39,7 +106,7 @@ app.get("/work", (req, res) => {
 
 app.get("/dummy", (req, res) => {
     res.render("dummyLayout");
-})
+});
 
 app.listen(3000, () => {
     console.log("Server Started");
